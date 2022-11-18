57f73cdd0da92d722362274a6763b09b124cd0c9
Yungxin <101496626+shinyungxin@users.noreply.github.com>
2022-11-19 01:40:14 +0800

    update ryoko with flirting ability

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ import discord
 import requests
 
 TOKEN = ""
-INSULT_API = "https://insult.mattbas.org/api/insult"
+
+INSULT_API = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
+INSULT_KEY = "insult"
+PICK_UP_API = "https://getpickuplines.herokuapp.com/lines/random"
+PICK_UP_KEY = "line"
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -10,9 +14,9 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 
-def get_insult_quote():
-    response = requests.get(INSULT_API)
-    return response.content.decode("utf-8")
+def get_quote(api_url, key):
+    response = requests.get(api_url)
+    return response.json()[key]
 
 
 @client.event
@@ -29,8 +33,10 @@ async def on_message(message):
         await message.channel.send('Yo simp.')
     elif message.content.startswith('$bye'):
         await message.channel.send('Get lost.')
-    elif message.content.startswith('$insult me'):
-        await message.channel.send(get_insult_quote())
+    elif message.content.startswith('$insult'):
+        await message.channel.send(get_quote(INSULT_API, INSULT_KEY))
+    elif message.content.startswith('$flirt'):
+        await message.channel.send(get_quote(PICK_UP_API, PICK_UP_KEY))
     return
 
 client.run(TOKEN)
--- test.py
+++ test.py
@@ -1,10 +1,18 @@
 import requests
+import json
 
-INSULT_API = "https://insult.mattbas.org/api/insult"
+INSULT_API = "https://evilinsult.com/generate_insult.php?lang=en&type=json"
+INSULT_KEY = "insult"
+PICK_UP_API = "https://getpickuplines.herokuapp.com/lines/random"
+PICK_UP_KEY = "line"
 
-def get_quote():
-    response = requests.get(INSULT_API)
-    response_text = response.content.decode("utf-8")
-    print(response_text)
 
-get_quote()
+def get_quote(api_url, key):
+    response = requests.get(api_url)
+    # response_text = json.load(response.json())
+    respond = response.json()[key]
+    print(respond)
+    print(type(respond))
+
+
+get_quote(PICK_UP_API, PICK_UP_KEY)
